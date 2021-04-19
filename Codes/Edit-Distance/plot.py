472aee86d10407f28bbb9f4e8589454bfc88155b
He Tianyao <45009029+hotelll@users.noreply.github.com>
2021-04-19 11:19:36 +0800

    Update plot.py

--- Codes/Edit-Distance/plot.py
+++ Codes/Edit-Distance/plot.py
@@ -8,6 +8,6 @@ y_3 = np.log(1+np.exp(-x))
 plt.plot(x, y_1, label='linear')
 plt.plot(x, y_2, label='reverse')
 plt.plot(x, y_3, label='log-exponential')
-plt.title("Transformation Function") #添加标题
+plt.title("Transformation Function")
 plt.legend()
 plt.show()
